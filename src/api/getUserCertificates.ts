c5157baac2530a023bc0be1090b7b1f8c12ed4db
Hazrat Gadjikerimov <hazratgs@gmail.com>
2020-10-20 18:13:51 +0300

    Deleted by default store

--- src/api/getUserCertificates.ts
+++ src/api/getUserCertificates.ts
@@ -35,11 +35,7 @@ export const getUserCertificates = _afterPluginsLoaded((resetCache: boolean = fa
       try {
         void (
           __cadesAsyncToken__ +
-          cadesStore.Open(
-            cadesplugin.CAPICOM_CURRENT_USER_STORE,
-            cadesplugin.CAPICOM_MY_STORE,
-            cadesplugin.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED,
-          )
+          cadesStore.Open()
         );
       } catch (error) {
         console.error(error);
